313bc978abc454451c7029f3f77f0b985c11f917
Anderson Gomez Gomez <anderggomez96@gmail.com>
2025-01-13 08:00:53 -0500

    Actualización de AppiumBasics.java para mejorar la funcionalidad de pruebas, incluyendo la optimización de los métodos de configuración y la corrección de errores menores.

--- demo/src/test/java/com/example/AppiumBasics.java
+++ demo/src/test/java/com/example/AppiumBasics.java
@@ -2,7 +2,10 @@ package com.example;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import io.appium.java_client.AppiumBy;
 
 public class AppiumBasics extends BaseTest {
@@ -13,7 +16,17 @@ public class AppiumBasics extends BaseTest {
         //AndroidDriver, IOSDriver
         // Appium Code -> Appium Server -> Mobile Device -> App -> Actions
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
 
+        // Se realiza una comparación de texto
+        String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitle, "WiFi settings");
+
+
+        driver.findElement(By.id("android:id/edit")).sendKeys("Wifi Gomez");
+        driver.findElements(By.className("android.widget.Button")).get(1).click();
 
     }
 }
