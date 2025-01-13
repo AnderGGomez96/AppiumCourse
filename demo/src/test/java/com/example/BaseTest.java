e2817297b13dcaa720667d16ef37a59c118148e9
Anderson Gomez Gomez <anderggomez96@gmail.com>
2025-01-13 08:00:39 -0500

    Configure Appium server and driver with implicit wait

--- demo/src/test/java/com/example/BaseTest.java
+++ demo/src/test/java/com/example/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -20,21 +21,29 @@ public class BaseTest {
 
     @BeforeClass
     public void ConfigureAppiumServer() throws MalformedURLException, URISyntaxException {
+
+        // Se configura el servicio de Appium
         service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\ASUS\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
         .withIPAddress("127.0.0.1")
         .usingPort(4723).build();
 
+        // Se inicia el servicio de Appium
         service.start();
 
+        // Se configura el driver de Appium
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("motorola moto g31");
         options.setApp("C:\\Users\\ASUS\\Documents\\Appium\\AppiumCourse\\demo\\src\\test\\java\\com\\example\\resources\\ApiDemos-debug.apk");
 
         driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+
+        // Se configura el tiempo de espera implicito
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterClass
     public void tearDowm() {
+        // Se cierra el driver y el servicio de Appium
         driver.quit();
         service.stop();
     }
